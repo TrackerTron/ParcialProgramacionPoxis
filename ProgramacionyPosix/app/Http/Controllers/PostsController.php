e213bf2b17febab8ea0a85e372d039bad52437af
IanAlexanderGrituz <147834955+IanAlexanderGrituz@users.noreply.github.com>
2023-11-02 16:44:43 -0300

    se ha creado el register y funciona con exito

--- ProgramacionyPosix/app/Http/Controllers/PostsController.php
+++ ProgramacionyPosix/app/Http/Controllers/PostsController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class PostsController extends Controller
 {
-    //
+
+    
+    public function register(Request $request)
+    {
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+
+        return redirect("/");
+    }
 }
