e257fa63da0484ec8111fd205c5423c30fbecb6c
IanAlexanderGrituz <147834955+IanAlexanderGrituz@users.noreply.github.com>
2023-11-15 19:31:38 -0300

    Implementación de CRUD y Asociación de Usuarios en API de Tareas
    Desarrollé el controlador TaskController para gestionar tareas, agregando operaciones CRUD. Asocié tareas a usuarios autenticados, permitiendo la creación, modificación, búsqueda y eliminación de tareas. Configuré rutas en routes/api.php para habilitar estas operaciones.

--- taskApi/app/Http/Controllers/TaskController.php
+++ taskApi/app/Http/Controllers/TaskController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
-class TaskController extends Controller
-{
-    //
+class TaskController extends Controller {
+
+    public function index() {
+        return Task::all();
+    }
+
+    public function show(Task $task) {
+        return $task;
+    }
+
+    public function store(Request $request) {
+
+        $task = Task::create($request->only(['title', 'description', 'author_id', 'state_id']));
+
+        $task->author_id = auth()->user()->id;
+        $task->save();
+        return response()->json($task, 201);
+    }
+
+    public function update(Request $request, Task $task) {
+        $task->update($request->only(['title', 'description', 'state_id']));
+        return response()->json($task, 200);
+    }
+
+    public function destroy(Task $task) {
+        $task->delete();
+        return response()->json(null, 204);
+    }
 }
