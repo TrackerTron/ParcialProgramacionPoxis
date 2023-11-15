66eba014c9e1520cf1b2826beea8332c968c2c81
IanAlexanderGrituz <147834955+IanAlexanderGrituz@users.noreply.github.com>
2023-11-15 18:24:43 -0300

    Migraciones con contenido
    Se le ha agregado contenido a las Migraciones de la API Task
    
    Definimos migraciones en Laravel para tres tablas: tasks, authors, y states. La migración de tasks incluye campos como título, descripción, autor, estado y ruta del archivo de texto. Las migraciones de authors y states contienen campos para nombres. Estas migraciones permiten gestionar tareas con autores, estados y archivos adjuntos.

--- taskApi/database/migrations/2023_11_15_211831_create_tasks_table.php
+++ taskApi/database/migrations/2023_11_15_211831_create_tasks_table.php
@@ -9,11 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up() {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('author_id');
+            $table->unsignedBigInteger('state_id');
+            $table->string('text_file_path')->nullable();
             $table->timestamps();
+    
+            // Definir restricciones de clave externa
+            $table->foreign('author_id')->references('id')->on('authors');
+            $table->foreign('state_id')->references('id')->on('states');
         });
     }
 
--- taskApi/database/migrations/2023_11_15_211840_create_authors_table.php
+++ taskApi/database/migrations/2023_11_15_211840_create_authors_table.php
@@ -6,20 +6,15 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('authors', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+  
+public function up() {
+    Schema::create('authors', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->timestamps();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('authors');
