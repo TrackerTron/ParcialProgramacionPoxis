06865f9a726e3292dc2ca965dc4a7330c82cdbf6
IanAlexanderGrituz <147834955+IanAlexanderGrituz@users.noreply.github.com>
2023-11-17 10:48:29 -0300

    Se modifico el api.php
    Se cambiaron las rutas  para facilitar el testing

--- taskApi/routes/api.php
+++ taskApi/routes/api.php
@@ -3,14 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 use App\Http\Controllers\TaskController;
 
-Route::get('/tasks', [TaskController::class, 'index']);       
-Route::get('/tasks/{task}', [TaskController::class, 'show']);  
-Route::post('/tasks', [TaskController::class, 'store']);      
-Route::put('/tasks/{task}', [TaskController::class, 'update']); 
-Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); 
+Route::prefix('tasks')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');        // Mostrar todas las tareas
+    Route::get('/show/{task}', [TaskController::class, 'show'])->name('tasks.show'); // Mostrar una tarea por ID
+    Route::post('/create', [TaskController::class, 'store'])->name('tasks.store');   // Crear una nueva tarea
+    Route::put('/update/{task}', [TaskController::class, 'update'])->name('tasks.update'); // Actualizar una tarea
+    Route::delete('/delete/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy'); // Eliminar una tarea
+});
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
